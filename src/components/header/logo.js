9ba3545e417a3cc5ebfd0bb17d592e742e886fc9
muzafferyenice <muzafferyenicet@gmailcom>
2022-09-17 20:04:42 +0300

    v3

--- src/components/header/logo.js
+++ src/components/header/logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
       <span className="bg-dark-2 rounded-pill p-2 mb-lg-1 d-none d-lg-inline-block">
         <img
           className="img-fluid rounded-pill d-block"
-          src="images/profile.jpg.jfif"
+          src="images/Muzafferyenice.png"
           title="I'm Muzaffer"
           alt=""
         />
